3a29684d00f8f22ba624b97d199ba1c7d7b0ec83
hee-bin <gmlqls4732@naver.com>
2024-06-10 12:04:11 +0900

    [adate] recommand deatil

--- app/recommend/[id]/page.jsx
+++ app/recommend/[id]/page.jsx
@@ -20,15 +20,9 @@ const EstimateCardDetail = ({ params }) => {
 
   return (
     <div className="flex flex-col items-center justify-center p-8">
-      <div className="flex items-center justify-center mb-8">
-        <div>
-          <h1 className="text-3xl font-bold">{card.title}</h1>
-          <p className="text-lg text-gray-700 mt-2">{description}</p>
-        </div>
-      </div>
       <div className="flex w-full max-w-4xl">
         <div className="flex-1 pr-8">
-          <img
+          <Image
             src="https://nextui.org/images/hero-card-complete.jpeg"
             alt="Detail Image"
             width={500}
@@ -37,6 +31,8 @@ const EstimateCardDetail = ({ params }) => {
           />
         </div>
         <div className="flex-1 pl-8">
+          <h1 className="text-3xl font-bold">{card.title}</h1>
+
           <h2 className="text-2xl font-semibold mb-4">{description}</h2>
           <div className="grid grid-cols-1 gap-2">
             {Object.entries(details).map(([key, value]) => (
--- components/communitycard.jsx
+++ components/communitycard.jsx
@@ -1,16 +1,16 @@
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
 import NextLink from "next/link";
-import  ComponentTable  from "@/components/componenttable";
-const CommunityCard = ({id, title, description}) => {
+import ComponentTable from "@/components/ComponentTable";
+const CommunityCard = ({ id, title, description }) => {
   return (
     <NextLink href={`/community/${id}`} passHref>
-      <Card as="a" className="py-4" style={{ width: '360px', height: '560px' }}>
+      <Card as="a" className="py-4" style={{ width: "360px", height: "560px" }}>
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
-        <h4 className="font-bold text-large">{title}</h4>
+          <h4 className="font-bold text-large">{title}</h4>
         </CardHeader>
         <CardBody className="overflow-visible py-2 items-center">
-          <ComponentTable/>
+          <ComponentTable />
         </CardBody>
       </Card>
     </NextLink>
