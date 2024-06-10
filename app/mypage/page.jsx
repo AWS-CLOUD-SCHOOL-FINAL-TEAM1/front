0f9abc82a0043433615918e3d280255a18cbf99e
Choi Sunhong <123724883+Choisunhong@users.noreply.github.com>
2024-06-10 14:29:33 +0900

    [fix]buttonadd

--- app/mypage/page.jsx
+++ app/mypage/page.jsx
@@ -2,6 +2,7 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import MypageCard from "@/components/MypageCard";
+import { Button } from "@nextui-org/button";
 
 const cardData = [
   {
@@ -22,12 +23,17 @@ const cardData = [
 
 export default function Mypage() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-full">
-      <div className="inline-block max-w-lg text-center">
-        <h1 className={title({ color: "" })}>마이페이지&nbsp;</h1>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-full w-full">
+      <div className="flex items-center justify-center w-full max-w-lg relative">
+        <h1 className={`${title({ color: "" })} absolute left-1/2 transform -translate-x-1/2`}>
+          마이페이지&nbsp;
+        </h1>
+        <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg ml-auto">
+          견적추가
+        </Button>
       </div>
       <div
-        className="grid gap-x-8 gap-y-4 grid-cols-3 mt-8 overflow-y-auto hide-scrollbar"
+        className="grid gap-x-8 gap-y-4 grid-cols-3 mt-8 overflow-y-auto hide-scrollbar w-full"
         style={{ height: "calc(100vh - 16rem)" }}
       >
         {cardData.map((card) => (
