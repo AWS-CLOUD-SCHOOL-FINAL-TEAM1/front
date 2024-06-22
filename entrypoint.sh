a3db0fe4caf086acdf6c1b82e298f80b028e3091
hee-bin <gmlqls4732@naver.com>
2024-06-22 16:29:14 +0900

    entrypoint source to dot

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,11 @@
 # Load environment variables from .env.local
 if [ -f /my-space/.env.local ]; then
   set -o allexport
-  source /my-space/.env.local
+  . /my-space/.env.local
   set +o allexport
 fi
+
+echo "Environment variables loaded:"
+env | grep NEXT_PUBLIC
 
 exec "$@"
