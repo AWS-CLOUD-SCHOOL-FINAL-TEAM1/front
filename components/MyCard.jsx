86f15582205ffaaf75e9dfa1a512dbe0b70b4d39
hee-bin <gmlqls4732@naver.com>
2024-06-28 15:15:25 +0900

    component card ui

--- components/MyCard.jsx
+++ components/MyCard.jsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
 import HeartButton from "@/components/HeartButton";
-import { FaArrowUp, FaArrowDown } from "react-icons/fa"; // 추가
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
-// MyCard 컴포넌트: 개별 컴포넌트를 카드 형식으로 렌더링
 const MyCard = ({
   id,
   title,
@@ -17,12 +16,10 @@ const MyCard = ({
   isFavorite,
   onAlarmClick,
 }) => {
-  // 하트 버튼 클릭 시 호출되는 함수
   const handleButtonClick = (e) => {
-    e.preventDefault(); // 기본 동작 중지 (링크 클릭 중지)
-    e.stopPropagation(); // 카드 클릭 이벤트가 실행되지 않도록 중지
-    console.log("Heart button clicked"); // 로그 추가
-    onAlarmClick(id, componentType, isFavorite); // 알람 모달 열기 함수 호출
+    e.preventDefault();
+    e.stopPropagation();
+    onAlarmClick(id, componentType, isFavorite);
   };
 
   const priceTrend = price < avgPriceLast45Days ? "down" : "up";
@@ -30,7 +27,8 @@ const MyCard = ({
     priceTrend === "down" ? "text-green-500" : "text-orange-500";
   const TrendIcon = priceTrend === "down" ? FaArrowDown : FaArrowUp;
 
-  // isFavorite을 제외한 스펙 필터링
+  const priceDifference = Math.abs(price - avgPriceLast45Days).toLocaleString();
+
   const filteredSpecs = specs.filter(
     (spec) =>
       !spec.toLowerCase().includes("isfavorite") &&
@@ -48,7 +46,6 @@ const MyCard = ({
         <Card
           className="p-3 cursor-pointer"
           style={{ width: "100%", height: "100%" }}
-          onClick={() => console.log("Card clicked")} // 로그 추가
         >
           <div className="flex justify-between">
             <h4 className="font-bold">{title}</h4>
@@ -59,12 +56,14 @@ const MyCard = ({
             <Image src={imageUrl} alt={title} className="rounded-xl" />
             <div className="mt-4">
               <div className="flex items-center">
-                <p className="mt-2 font-bold text-blue-600">{price}₩</p>
+                <p className="mt-2 font-bold text-blue-600">
+                  {price.toLocaleString()}₩
+                </p>
                 <TrendIcon className={`ml-2 ${trendColor}`} />
-                <p className={`ml-2 ${trendColor}`}>({avgPriceLast45Days}₩)</p>
+                <p className={`ml-2 ${trendColor}`}>({priceDifference}₩)</p>
               </div>
               <p className="text-gray-500 text-sm mt-1">
-                45일 평균 가격: {avgPriceLast45Days}₩
+                45일 평균 가격: {avgPriceLast45Days.toLocaleString()}₩
               </p>
               <table className="w-full table-auto text-left border-collapse mt-2">
                 <tbody>
