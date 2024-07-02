20dadd39fe73deed16e9be6e2a80ea31d26f492d
hee-bin <gmlqls4732@naver.com>
2024-07-02 12:08:09 +0900

    prerender error test

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -15,27 +15,18 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const code = searchParams.get("code") as string;
 
-    console.log("Request Origin:", REDIRECT_SIGNIN);
-    console.log("Request Search Params:", searchParams.toString());
-    console.log("Authorization Code:", code);
-
     if (!code) {
       const error = searchParams.get("error");
-      console.log("Error received:", error);
       return NextResponse.json({ error: error || "Unknown error" });
     }
 
     const authorizationHeader = `Basic ${Buffer.from(`${APP_CLIENT_ID}:${APP_CLIENT_SECRET}`).toString("base64")}`;
-
     const requestBody = new URLSearchParams({
       grant_type: "authorization_code",
       client_id: APP_CLIENT_ID as string,
       code,
       redirect_uri: `${REDIRECT_SIGNIN}/api/auth/callback`,
     });
-
-    console.log("Authorization Header:", authorizationHeader);
-    console.log("Request Body:", requestBody.toString());
 
     const res = await fetch(`${COGNITO_DOMAIN}/oauth2/token`, {
       method: "POST",
@@ -48,49 +39,35 @@ export async function GET(request: NextRequest) {
 
     const data = await res.json();
 
-    console.log("Response Status:", res.status);
-    console.log("Response Data:", data);
-
     if (!res.ok) {
-      console.log("Error Response:", data);
       return NextResponse.json({
         error: data.error,
         error_description: data.error_description,
       });
     }
 
-    const decodedIdToken: any = jwtDecode(data.id_token); // ID 토큰을 디코딩
-    console.log("Decoded ID Token:", decodedIdToken); // 디코딩된 토큰을 콘솔에 출력
+    const decodedIdToken: any = jwtDecode(data.id_token);
 
     const cookieStore = cookies();
     cookieStore.set("id_token", data.id_token);
     cookieStore.set("access_token", data.access_token);
     cookieStore.set("refresh_token", data.refresh_token);
 
-    console.log("Cookies set successfully");
-
-    // get_user_info API 호출
     const userInfoResponse = await fetch(`${API_KEY}/login/user-info/`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
     });
 
     const userInfo = await userInfoResponse.json();
 
     if (!userInfoResponse.ok) {
-      console.log("Error fetching user info:", userInfo);
       return NextResponse.json({
         error: userInfo.error || "Failed to fetch user info",
       });
     }
 
-    console.log("User Info:", userInfo);
-
     return NextResponse.redirect(new URL("/", request.nextUrl));
   } catch (error) {
-    console.log("Catch Error:", error);
     return NextResponse.json({ error });
   }
 }
