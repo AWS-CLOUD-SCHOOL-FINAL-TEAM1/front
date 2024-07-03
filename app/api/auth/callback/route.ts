ff7491c5caa6a37e21510d389c93cc5a904f99be
hee-bin <gmlqls4732@naver.com>
2024-07-03 10:04:23 +0900

    revoke

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -2,16 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { jwtDecode } from "jwt-decode";
 
-const { COGNITO_DOMAIN, APP_CLIENT_ID, APP_CLIENT_SECRET, API_KEY } =
-  process.env;
+const {
+  COGNITO_DOMAIN,
+  APP_CLIENT_ID,
+  APP_CLIENT_SECRET,
+  API_KEY,
+  REDIRECT_SIGNIN,
+} = process.env;
 
 export async function GET(request: NextRequest) {
   try {
-    const origin = request.nextUrl.origin;
     const searchParams = request.nextUrl.searchParams;
     const code = searchParams.get("code") as string;
 
-    console.log("Request Origin:", origin);
+    console.log("Request Origin:", REDIRECT_SIGNIN);
     console.log("Request Search Params:", searchParams.toString());
     console.log("Authorization Code:", code);
 
@@ -20,16 +24,19 @@ export async function GET(request: NextRequest) {
       console.log("Error received:", error);
       return NextResponse.json({ error: error || "Unknown error" });
     }
+
     const authorizationHeader = `Basic ${Buffer.from(`${APP_CLIENT_ID}:${APP_CLIENT_SECRET}`).toString("base64")}`;
+
     const requestBody = new URLSearchParams({
       grant_type: "authorization_code",
       client_id: APP_CLIENT_ID as string,
       code,
-      redirect_uri: `${origin}/api/auth/callback`,
+      redirect_uri: `${process.env.REDIRECT_SIGNIN}/api/auth/callback`,
     });
 
     console.log("Authorization Header:", authorizationHeader);
     console.log("Request Body:", requestBody.toString());
+
     const res = await fetch(`${COGNITO_DOMAIN}/oauth2/token`, {
       method: "POST",
       headers: {
@@ -38,9 +45,12 @@ export async function GET(request: NextRequest) {
       },
       body: requestBody,
     });
+
     const data = await res.json();
+
     console.log("Response Status:", res.status);
     console.log("Response Data:", data);
+
     if (!res.ok) {
       console.log("Error Response:", data);
       return NextResponse.json({
@@ -48,24 +58,24 @@ export async function GET(request: NextRequest) {
         error_description: data.error_description,
       });
     }
+
     const decodedIdToken: any = jwtDecode(data.id_token); // ID 토큰을 디코딩
     console.log("Decoded ID Token:", decodedIdToken); // 디코딩된 토큰을 콘솔에 출력
+
     const cookieStore = cookies();
     cookieStore.set("id_token", data.id_token);
     cookieStore.set("access_token", data.access_token);
     cookieStore.set("refresh_token", data.refresh_token);
+
     console.log("Cookies set successfully");
 
     // get_user_info API 호출
-    const userInfoResponse = await fetch(
-      `${process.env.API_KEY}/login/user-info/`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const userInfoResponse = await fetch(`${API_KEY}/login/user-info/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     const userInfo = await userInfoResponse.json();
 
@@ -75,7 +85,9 @@ export async function GET(request: NextRequest) {
         error: userInfo.error || "Failed to fetch user info",
       });
     }
+
     console.log("User Info:", userInfo);
+
     return NextResponse.redirect(new URL("/", request.nextUrl));
   } catch (error) {
     console.log("Catch Error:", error);
--- app/api/auth/google-sign-in/route.ts
+++ app/api/auth/google-sign-in/route.ts
@@ -1,5 +1,3 @@
-// /app/api/auth/google-sign-in/route.ts
-
 import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto";
 
@@ -7,17 +5,27 @@ const { COGNITO_DOMAIN, APP_CLIENT_ID } = process.env;
 
 export async function GET(request: NextRequest) {
   const authorizeParams = new URLSearchParams();
-  const origin = request.nextUrl.origin;
+  const origin = process.env.SPD_FE_SERVICE_HOST;
+
+  console.log("Request Origin:", origin); // origin 값을 로그로 출력하여 확인합니다.
+  console.log("COGNITO_DOMAIN:", COGNITO_DOMAIN); // COGNITO_DOMAIN 값을 로그로 출력하여 확인합니다.
+  console.log("APP_CLIENT_ID:", APP_CLIENT_ID); // APP_CLIENT_ID 값을 로그로 출력하여 확인합니다.
 
   const state = crypto.randomBytes(16).toString("hex");
 
   authorizeParams.append("response_type", "code");
   authorizeParams.append("client_id", APP_CLIENT_ID as string);
-  authorizeParams.append("redirect_uri", `${origin}/api/auth/callback`);
+  authorizeParams.append(
+    "redirect_uri",
+    `${process.env.REDIRECT_SIGNIN}/api/auth/callback`
+  );
   authorizeParams.append("state", state);
   authorizeParams.append("identity_provider", "Google");
   authorizeParams.append("scope", "profile email openid");
+
   const redirectUrl = `${COGNITO_DOMAIN}/oauth2/authorize?${authorizeParams.toString()}`;
+
   console.log("Redirecting to:", redirectUrl);
+
   return NextResponse.redirect(redirectUrl);
 }
