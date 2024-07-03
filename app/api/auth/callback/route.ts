9e65bdce1b826e7630417bcc62479bfa50862cdc
hee-bin <gmlqls4732@naver.com>
2024-07-03 10:10:56 +0900

    revoke

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -24,19 +24,16 @@ export async function GET(request: NextRequest) {
       console.log("Error received:", error);
       return NextResponse.json({ error: error || "Unknown error" });
     }
-
     const authorizationHeader = `Basic ${Buffer.from(`${APP_CLIENT_ID}:${APP_CLIENT_SECRET}`).toString("base64")}`;
-
     const requestBody = new URLSearchParams({
       grant_type: "authorization_code",
       client_id: APP_CLIENT_ID as string,
       code,
-      redirect_uri: `${process.env.REDIRECT_SIGNIN}/api/auth/callback`,
+      redirect_uri: `${REDIRECT_SIGNIN}/api/auth/callback`,
     });
 
     console.log("Authorization Header:", authorizationHeader);
     console.log("Request Body:", requestBody.toString());
-
     const res = await fetch(`${COGNITO_DOMAIN}/oauth2/token`, {
       method: "POST",
       headers: {
@@ -45,12 +42,9 @@ export async function GET(request: NextRequest) {
       },
       body: requestBody,
     });
-
     const data = await res.json();
-
     console.log("Response Status:", res.status);
     console.log("Response Data:", data);
-
     if (!res.ok) {
       console.log("Error Response:", data);
       return NextResponse.json({
@@ -58,15 +52,12 @@ export async function GET(request: NextRequest) {
         error_description: data.error_description,
       });
     }
-
     const decodedIdToken: any = jwtDecode(data.id_token); // ID 토큰을 디코딩
     console.log("Decoded ID Token:", decodedIdToken); // 디코딩된 토큰을 콘솔에 출력
-
     const cookieStore = cookies();
     cookieStore.set("id_token", data.id_token);
     cookieStore.set("access_token", data.access_token);
     cookieStore.set("refresh_token", data.refresh_token);
-
     console.log("Cookies set successfully");
 
     // get_user_info API 호출
@@ -85,9 +76,7 @@ export async function GET(request: NextRequest) {
         error: userInfo.error || "Failed to fetch user info",
       });
     }
-
     console.log("User Info:", userInfo);
-
     return NextResponse.redirect(new URL("/", request.nextUrl));
   } catch (error) {
     console.log("Catch Error:", error);
