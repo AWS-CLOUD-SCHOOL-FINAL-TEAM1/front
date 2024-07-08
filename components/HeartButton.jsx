b25f09f45087aee5cb1a99a242eac49f8e23df8b
hee-bin <gmlqls4732@naver.com>
2024-07-08 15:45:33 +0900

    [update]alarm btn color

--- components/HeartButton.jsx
+++ components/HeartButton.jsx
@@ -1,30 +1,41 @@
 import React from "react";
 import { Button } from "@nextui-org/button";
-import { FaBell, FaBellSlash } from "react-icons/fa"; // react-icons에서 아이콘 가져오기
+import { FaBell, FaBellSlash } from "react-icons/fa";
 
-// HeartButton 컴포넌트: 좋아요/알람 버튼
 const HeartButton = ({ isFavorite, onClick }) => {
-  // 버튼 클릭 시 호출되는 함수
   const handleClick = (e) => {
-    e.preventDefault(); // 기본 동작 중지 (링크 클릭 중지)
-    e.stopPropagation(); // 클릭 이벤트가 부모 컴포넌트로 전파되지 않도록 중지
-    console.log("HeartButton clicked:", isFavorite); // 로그 추가
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("HeartButton clicked:", isFavorite);
     if (onClick) {
-      onClick(e); // 전달된 onClick 함수 실행
+      onClick(e);
     } else {
       console.error("onClick handler is not provided");
     }
   };
 
+  const gradientStyle = isFavorite
+    ? {
+        background: "#b4ff5e",
+        background: "-webkit-linear-gradient(219deg, #b4ff5e 0%, #2263f0 100%)",
+        background: "linear-gradient(219deg, #b4ff5e 0%, #2263f0 100%)",
+        color: "white",
+      }
+    : {
+        background: "transparent",
+        border: "1px solid rgba(0, 98, 244, 0.76)",
+        color: "rgba(0, 98, 244, 0.76)",
+      };
+
   return (
     <Button
-      color={isFavorite ? "success" : "default"}
       auto
       ghost={!isFavorite}
-      onClick={handleClick} // 클릭 이벤트 핸들러
-      size="sm" // 버튼 크기 작게 설정
+      onClick={handleClick}
+      size="sm"
+      style={gradientStyle} // Apply gradient style
     >
-      {isFavorite ? <FaBell /> : <FaBellSlash />} {/* 아이콘으로 변경 */}
+      {isFavorite ? <FaBell /> : <FaBellSlash />}
     </Button>
   );
 };
